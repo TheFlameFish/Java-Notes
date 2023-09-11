7863604d484c25bbbc260549f718cd5e641cd23f
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2023-09-11 16:30:15 -0700

    Added input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -11,5 +13,10 @@ public class Main {
         int ohNoInt = (int) (funkyFloat+0.01f);// Add 0.01 to funkyFloat, then convert the sum to an int
         System.out.println(ohNoInt);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String input = scanner.nextLine();
+
+        System.out.println("Hello, "+input+"!");
     }
 }
